40081cf06162baf7915f8c92ae406e8bacb51225
dianabreik <diana.sam341@gmail.com>
2023-08-31 16:44:30 +0200

    Vergleichen mittels der Methode "equals()".

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,8 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+
+        //Installieren der Infos der Fahrzeuge und sie ausgeben.
         Fahrzeug fahrzeug1 = new Fahrzeug("AA","AA",2015);
         System.out.println(fahrzeug1);
         Auto auto1 = new Auto("BB","BB",2020,4);
@@ -10,5 +12,18 @@ public class Main {
         System.out.println(motorrad1);
         /*motorrad1.modell ="xxx";
         System.out.println(motorrad1);*/
+
+        System.out.println("--------------------^-^-----------------------");
+
+        // Vergleichen zwischen 2 Autos also die Objekte auto1 und auto2, mithilfe der Methode equals()
+        Auto auto2 = new Auto("BB","BB",2020,4);
+        System.out.println("Q: Sind die beiden Autos gleich?");
+        if(auto1.equals(auto2)){
+            System.out.println("A: Jaa, sie sind gleich :)");
+        }
+        else {
+            System.out.println("A: Nein, sie sind verschieden :)");
+        }
+        ;
     }
 }
